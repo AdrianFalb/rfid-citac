4a83d4bd6486a63090d2e1b8bfc85f3278963f33
Samwise101 <samuel.palenik20@gmail.com>
2023-12-19 17:55:47 +0100

    initial commiz

---
+++ ILI9163/font.h
@@ -0,0 +1,13 @@
+/*
+ * font.h
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: u
+ */
+
+#ifndef FONT_H_
+#define FONT_H_
+
+
+
+#endif /* FONT_H_ */
---
+++ ILI9163/ili9163.c
@@ -0,0 +1,8 @@
+/*
+ * ili9163.c
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: u
+ */
+
+
